c0b1ac0cf8be862f526e25bbbad57ec99d518ddc
Lluc4s <phuttiphongbank@gmail.com>
2024-11-24 16:19:31 +0000

    update create_user()

--- tutorials/management/commands/seed.py
+++ tutorials/management/commands/seed.py
@@ -1,17 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from tutorials.models import User
+from tutorials.models import User, Tutor, Tutee
 
 import pytz
 from faker import Faker
 from random import randint, random
 
+janedoe_languages = ("Python", "Java")
+
 user_fixtures = [
     {'username': '@johndoe', 'email': 'john.doe@example.org', 'first_name': 'John', 'last_name': 'Doe', 'is_superuser': True, 'is_staff': True, 'is_tutor': False},
-    {'username': '@janedoe', 'email': 'jane.doe@example.org', 'first_name': 'Jane', 'last_name': 'Doe', 'is_superuser': False, 'is_staff': False, 'is_tutor': True},
+    {'username': '@janedoe', 'email': 'jane.doe@example.org', 'first_name': 'Jane', 'last_name': 'Doe', 'is_superuser': False, 'is_staff': False, 'is_tutor': True, 'languages_specialised': janedoe_languages},
     {'username': '@charlie', 'email': 'charlie.johnson@example.org', 'first_name': 'Charlie', 'last_name': 'Johnson',  'is_superuser': False, 'is_staff': False, 'is_tutor': False},
 ]
-
 
 class Command(BaseCommand):
     """Build automation command to seed the database."""
@@ -57,7 +58,7 @@ class Command(BaseCommand):
             pass
 
     def create_user(self, data):
-        User.objects.create_user(
+        user = User.objects.create_user(
             username=data['username'],
             email=data['email'],
             password=Command.DEFAULT_PASSWORD,
@@ -67,6 +68,14 @@ class Command(BaseCommand):
             is_staff=data['is_staff'],
             is_tutor=data['is_tutor']
         )
+
+        if data['is_tutor']:
+            Tutor.objects.create(
+                user=user,
+                languages_specialised=", ".join(data['languages_specialised'])
+            )
+        elif not data['is_staff']:
+            Tutee.objects.create(user=user)
 
 def create_username(first_name, last_name):
     return '@' + first_name.lower() + last_name.lower()
